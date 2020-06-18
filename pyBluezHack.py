b9951df3ab4444a54a6441c840c6ce00961ab3cc
Abel506 <agebre@sea.yearup.org>
2020-06-18 02:07:55 -0700

    PyBluez Script ITC 134

---
+++ pyBluezHack.py
@@ -0,0 +1,25 @@
+
+
+#this script that advertises a bluetooth low energy beacon for 15 seconds
+
+# creating a class library
+import time #<--- 1st party class module
+
+from bluetooth.ble import BeaconService #<--- 3rd party module 
+
+#create an instance of the object from the 3rd party class 
+
+service = BeaconService()
+
+#looking for a connection 
+service.start_advertising("11111111-2222-3333-4444-555555555555",1 , 1, 1, 200)#<--UUID
+
+#Break for 15min
+time . sleep(15)
+
+service.stop_advertisizing ()
+
+#print done
+print("Done.")
+
+
